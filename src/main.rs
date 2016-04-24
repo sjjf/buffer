27dd603b642aab19ff41cf6c42ac398ac0fc7aa2
Simon Fowler <simon.fowler@anu.edu.au>
2016-04-24 20:25:11 +1000

    Fully decouple read and write sides.
    This is achieved by splitting read and write into separate threads, with
    a protected buffer in between. At present there's no constraint on the
    size of the buffer.
    
    Testing with the demo code at
    https://www.idontplaydarts.com/static/bashserver.zip shows that this
    does indeed protect against the bash detection method described at
    https://www.idontplaydarts.com/2016/04/detecting-curl-pipe-bash-server-side/
    
    Don't pipe code downloaded from the Internet directly into bash! That's
    stupid. But if you want to decouple the sending process behaviour from the
    receiving process behaviour in a shell command this code does that.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::io::prelude::*;
 use std::io::{stdin, stdout, stderr};
 use std::io::{Error, ErrorKind};
+use std::thread;
 use std::sync::{Arc, Mutex};
 use std::collections::VecDeque;
 
@@ -10,14 +11,12 @@ use std::collections::VecDeque;
 // operations will grab the mutex, check the length against the maximum size,
 // if there's space push the new data and then release the mutex; pop
 // operations will grab the mutex, pop_front(), and then release the mutex. If
-// the pop returns None then it spins in a reasonably tight loop (but with a
-// small random delay to ensure that it doesn't block the push thread).
+// the pop returns None then it yields to (hopefully) allow the reader thread
+// to get more data
 
-// return data, or None if we read EOF
-//
-// We could return a Result, but I don't think EOF is an error . . .
 fn get_data() -> io::Result<Vec<u8>> {
     let mut data = vec![0; 1024];
+    // may block
     match stdin().read(&mut data[..]) {
         Ok(l) =>  match l {
                 0 => Err(Error::new(ErrorKind::Other, "EoF")),
@@ -32,6 +31,7 @@ fn get_data() -> io::Result<Vec<u8>> {
 
 // write the whole buffer, repeating if necessary
 fn put_data(data: Vec<u8>) -> io::Result<usize> {
+    // may block, but probably shouldn't
     match stdout().write(&data[..]) {
         Ok(l) => Ok(l),
         Err(error) => {
@@ -41,31 +41,94 @@ fn put_data(data: Vec<u8>) -> io::Result<usize> {
     }
 }
 
+#[derive(Clone)]
+enum QueueState {
+    Incomplete,
+    Complete,
+}
+
+// In something like C/C++/Python/whatever I'd embed the lock in the structure,
+// but I can't figure out how to do this in rust. That would allow me to keep
+// all the lock handling in the Queue impl block. Without it I have to do the
+// lock handling in the main code.
+struct Queue {
+    state: QueueState,
+    data: VecDeque<Vec<u8>>,
+}
+
+impl Queue {
+
+    fn new() -> Queue {
+        let q = Queue {
+            state: QueueState::Incomplete,
+            data: VecDeque::new(),
+        };
+        q
+    }
+}
+
 fn main() {
     // The idea here is that we read bytes on one side into a buffer, and on
     // the other we write from the buffer. When the buffer has no data we don't
     // write anything.
     //
-    // This requires non-blocking reads that feed as much data as is available
-    // into the buffer, and non-blocking writes to stdout, connected by a tight
-    // loop.
-    //
-    // Okay, since non-blocking I/O doesn't seem to be supported, I guess I'll
+    // Since non-blocking I/O doesn't seem to be supported in rust, I guess I'll
     // have to use separate read and write threads, connected by a protected
     // buffer . . .
-    loop {
-        match get_data() {
-            Ok(data) =>  match put_data(data) {
-                    Err(err) => write!(stderr(), "Write error! {}\n", err).unwrap(),
-                    Ok(_) => {},
-            },
-            Err(error) => match error.kind() {
-                ErrorKind::Other => {
-                    write!(stderr(), "Nothing left!\n").unwrap();
-                    break;
-                },
-                _ => write!(stderr(), "Unexpected Error {}", error).unwrap(),
+    let queue = Arc::new(Mutex::new(Queue::new()));
+    {
+        let queue = queue.clone();
+        thread::spawn(move || {
+            loop {
+                match get_data() {
+                    Ok(data) => {
+                        let mut queue = queue.lock().unwrap();
+                        queue.data.push_back(data);
+                    },
+                    Err(error) => match error.kind() {
+                        ErrorKind::Other => {
+                            let mut queue = queue.lock().unwrap();
+                            queue.state = QueueState::Complete;
+                            break;
+                        },
+                        _ => {
+                            write!(stderr(), "Unexpected Error {}", error)
+                                .expect("stderr failure");
+                        },
+                    }
+                }
             }
+        });
+    }
+    let queue = queue.clone();
+    loop {
+        // do this in a block because the MutexGuard type is RAII and unlocks
+        // when it goes out of scope
+        let result = {
+            let mut queue = queue.lock().unwrap();
+            queue.data.pop_front()
+        };
+        match result {
+            Some(data) =>  match put_data(data) {
+                Err(err) => {
+                    write!(stderr(), "Write error! {}\n", err)
+                        .expect("stderr failure");
+                },
+                Ok(_) => {},
+            },
+            None => {
+                // this is inelegant, but it works okay
+                let result = {
+                    let queue = queue.lock().unwrap();
+                    queue.state.clone()
+                };
+                match result {
+                    QueueState::Incomplete => thread::yield_now(),
+                    QueueState::Complete => {
+                        break;
+                    },
+                }
+            },
         }
     }
 }
